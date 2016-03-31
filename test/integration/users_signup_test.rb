599cfd221468ef4a8628c212f4df9cf205654c3c
Dongmin Lee <medi8600@gmail.com>
2016-03-31 11:24:50 -0500

    signup test modified

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,14 +3,14 @@ require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
-    assert_no_difference 'User.count' do
-      post users_path, user: {
-        name: "",
-        email: "user@invalid",
-        password: "some",
-        password_confirmation: "some"
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: {
+        name: "example user",
+        email: "user@example.com",
+        password: "something",
+        password_confirmation: "something"
       }
     end
-    assert_template 'users/new'
+    assert_template 'users/show'
   end
 end
